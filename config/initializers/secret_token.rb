842fe1fe664556361a43af786bc53b1c7621f83e
Hamilton <hamilton.oliveira@m2msolutions.com.br>
2013-12-06 16:38:34 -0300

    secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meuponto::Application.config.secret_key_base = '9dc632ed306371dc66f60f72fce98cd47b12e0f0b1a8ea372bbe109470811fd8953178da181b68055ad595c28b0287d705f5e9df347093b9c962978910f25e80'
